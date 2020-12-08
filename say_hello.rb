95566de398bb537929dc47219229a66b0b72c033
courserav <lvsmlit@gmail.com>
2020-12-08 13:03:23 -0500

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,11 @@
 # Build your say_hello method here
+
+def say_hello(name = "Ruby Programmer")
+    puts "Hello #{name}!"
+end
+
+#test name input
+say_hello("Gabriela")
+
+#test default name
+say_hello
